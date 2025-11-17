36396107551dd33826913d54b763b38a605965ae
Deepanshu yadav <deepanshu.yadav@sensationsolutions.in>
2025-11-17 21:47:08 +0530

    lates product details

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -2,6 +2,7 @@ import { Auth } from "../models/auth.schema.js";
 import { comparePassword, hashedPassword } from "../../utils/hashedPassword.js";
 
 import { genToken } from "../../utils/authToken.js";
+import jwt from "jsonwebtoken";
 
 export const signupController = async (req, res, next) => {
   try {
@@ -32,7 +33,7 @@ export const signupController = async (req, res, next) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      sameSite: "none",
+      sameSite: "strict",
       secure: false,
       maxAge: 24 * 60 * 60 * 1000,
     });
@@ -65,21 +66,68 @@ export const loginController = async (req, res, next) => {
     const isPassword = await comparePassword(password, isEmailExist.password);
     if (isPassword) {
       const token = await genToken(isEmailExist._id, isEmailExist.role);
+
       res.cookie("token", token, {
         httpOnly: true,
-        sameSite: "none",
+        sameSite: "strict",
         secure: false,
         maxAge: 24 * 60 * 60 * 1000,
       });
       return res.status(200).json({
         message: "Login sucessfully",
-        data: isEmailExist._id,
+        data: {
+          id: isEmailExist._id,
+          name: isEmailExist.userName,
+        },
       });
     } else {
       return res.status(400).json({
         message: "Invalid Password",
       });
     }
+  } catch (err) {
+    return res.status(500).json({
+      message: err.message,
+    });
+  }
+};
+
+export const getMyProfile = async (req, res, next) => {
+  try {
+    const token = req.cookies?.token;
+    if (!token) {
+      return res.status(401).json({
+        message: "No token, Unauthorized",
+      });
+    }
+    const decoded = await jwt.verify(token, process.env.SECRET_KEY);
+    const user = await Auth.findById(decoded.id).select("-password");
+    if (!user) {
+      return res.status(400).json({
+        message: "No User Found",
+      });
+    }
+
+    return res.status(200).json({
+      data: user.userName,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      message: err.message,
+    });
+  }
+};
+
+export const logoutController = async (req, res, next) => {
+  try {
+    res.clearCookie("token", {
+      httpOnly: true,
+      sameSite: "strict",
+      secure: false,
+    });
+    return res.status(200).json({
+      message: "Logged Out Successfully",
+    });
   } catch (err) {
     return res.status(500).json({
       message: err.message,
--- api/routes/auth.router.js
+++ api/routes/auth.router.js
@@ -1,5 +1,7 @@
 import {
+  getMyProfile,
   loginController,
+  logoutController,
   signupController,
 } from "../controllers/auth.controller.js";
 import express from "express";
@@ -8,5 +10,7 @@ const router = express.Router();
 
 router.post("/signup", signupController);
 router.post("/signin", loginController);
+router.get("/profile", getMyProfile);
+router.get("/logOut", logoutController);
 
 export default router;
