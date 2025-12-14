cbc15d6c9f2d829c743fc98771234d593f7387c9
Deepanshu yadav <deepanshu.yadav@sensationsolutions.in>
2025-12-14 22:14:47 +0530

    fix: cors

--- app.js
+++ app.js
@@ -16,12 +16,16 @@ dotenv.config();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173", 
+      "https://gadget-dukan.vercel.app", 
+    ],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
 );
+
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
